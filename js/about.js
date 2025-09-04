75a5b8491177c122a39b0dbfc32878707062e04b
Shoon <emmagalitzine25@gmail.com>
2025-09-04 21:15:58 +0630

    Edited links

--- js/about.js
+++ js/about.js
@@ -1,3 +1,5 @@
+import "./index.js";
+
 const reviews = [
   {
     name: "Scarlett",
--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
 import "./componentsLoader.js";
 import "./fontsLoader.js";
 import "./starLoader.js";
-
-import "./about.js";
